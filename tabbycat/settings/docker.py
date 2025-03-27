ce77cd25ce070fac9f2a0621ff0e6bcc3571eb0b
websitysolutions <98515390+websitysolutions@users.noreply.github.com>
2025-03-27 13:10:02 +0300

    Update docker.py

--- tabbycat/settings/docker.py
+++ tabbycat/settings/docker.py
@@ -39,3 +39,13 @@ if bool(int(os.environ['DOCKER_REDIS'])) if 'DOCKER_REDIS' in os.environ else Fa
             },
         },
     }
+
+
+if os.environ.get('EMAIL_HOST', ''):
+    SERVER_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'root@localhost')
+    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'notconfigured@tabbycatsite')
+    EMAIL_HOST = os.environ['EMAIL_HOST']
+    EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+    EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+    EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+    EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'true').lower() == 'true'
